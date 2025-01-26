e9fff76390e22fe48598ad544c3132da64c337c3
pitroda-meet <mpitroda459@rku.ac.in>
2025-01-26 13:21:28 +0530

    last packege.json change

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    port: process.env.PORT || 3000, // Use the PORT environment variable or default to 3000
+    host: "0.0.0.0", // Expose the server to all network interfaces
+  },
+});
