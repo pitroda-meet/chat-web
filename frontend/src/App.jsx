771d8f41774a0930314def22829d23d7ae35a340
pitroda-meet <mpitroda459@rku.ac.in>
2025-03-27 17:27:01 +0530

    socket provide to app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,31 +1,23 @@
-import { useEffect } from "react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import HomePage from "./components/HomePage";
 import Login from "./components/Login";
 import Signup from "./components/Signup";
 import { useDispatch, useSelector } from "react-redux";
-import io from "socket.io-client";
-import { setSocket } from "./redux/socketSlice";
 import { setOnlineUsers } from "./redux/userSlice";
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <HomePage />,
-  },
-  {
-    path: "/signup",
-    element: <Signup />,
-  },
+import { useEffect } from "react";
+import { SocketProvider, useSocket } from "./SocketContext";
+import io from "socket.io-client";
 
-  {
-    path: "/login",
-    element: <Login />,
-  },
+const router = createBrowserRouter([
+  { path: "/", element: <HomePage /> },
+  { path: "/signup", element: <Signup /> },
+  { path: "/login", element: <Login /> },
 ]);
-function App() {
+
+function AppContent() {
   const { authUser } = useSelector((store) => store.user);
-  const { socket } = useSelector((store) => store.socket);
   const dispatch = useDispatch();
+  const { socket, setSocket } = useSocket();
 
   useEffect(() => {
     if (authUser) {
@@ -34,28 +26,34 @@ function App() {
         withCredentials: true,
       });
 
-      dispatch(setSocket(socket));
+      setSocket(socket);
       socket.on("getOnlineUsers", (onlineUsers) => {
         dispatch(setOnlineUsers(onlineUsers));
       });
       return () => {
         socket.close();
-        dispatch(setSocket(null));
+        setSocket(null);
       };
     } else {
       if (socket) {
         socket.close();
-        dispatch(setSocket(null));
+        setSocket(null);
         dispatch(setOnlineUsers([]));
       }
     }
   }, [authUser, dispatch]);
+
+  return <RouterProvider router={router} />;
+}
+
+function App() {
   return (
-    <>
+    <SocketProvider>
       <div className="flex items-center justify-center h-screen p-4">
-        <RouterProvider router={router} />
+        <AppContent />
       </div>
-    </>
+    </SocketProvider>
   );
 }
+
 export default App;
